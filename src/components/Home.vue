197cb9902ccfaf6bf64cf30fb623f1183b506622
Mozeeb Abdulha <mozeebabdulha@gmail.com>
2019-11-19 12:26:22 +0000

    Added Home Component

---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hero">
+      <h1 class="vue-title">Need some help to find your pet?</h1>
+
+        <table align="center">
+          <tr>
+            <td>
+              <p class="lead"><b><i>Time for a Result !!</i></b>
+              <p>Out with Boring Prumt - Giving has never been so easy.</p>
+              <p>Just click <a href ="/pet">here</a> to go to</p>
+              <p>the Pet page and report your Pet</p>
+            </td>
+            <td>
+              <img src="../assets/missingpet.gif" alt="description here" />
+            </td>
+          </tr>
+        </table>
+
+      <p></p>
+      <p class="lead">This is the homepage of your <b>MEVN</b> Web app</p>
+    </div>
+</template>
+
+<style>
+  .hero {
+    height: 100vh;
+    margin-top: 30px;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+  .hero .lead {
+    font-weight: 200;
+    font-size: 2.5rem;
+  }
+  #app1 {
+    width: 60%;
+    margin: 0 auto;
+  }
+  .vue-title {
+    font-size: 70pt;
+    margin-bottom: 10px;
+  }
+</style>
