d7505ec127e8e18b3404f92d1d186be8601047e5
Mozeeb Abdulha <mozeebabdulha@gmail.com>
2019-11-19 12:20:05 +0000

    Added new components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <b-navbar toggleable="md" variant="light" type="light">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <img src="./assets/paw.png" class="img-circle" style="padding: 5px">
+      <b-navbar-brand to="/">Missing Paws Web App</b-navbar-brand>
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item to="/#"><i class="fa fa-home" style="padding: 5px"> Home</i></b-nav-item>
+          <b-nav-item to="/pets"><i class="fa fa-list" style="padding: 5px"> Manage Pets</i></b-nav-item>
+          <b-nav-item to="/add"><i class="fa fa-plus" style="padding: 5px"> Add Pet</i></b-nav-item>
+          <b-nav-item to="/map"><i class="fa fa-globe" style="padding: 5px"> Map</i></b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/about"><i class="fa fa-info" style="padding: 5px"> About Us</i></b-nav-item>
+          <b-nav-item to="/contact"><i class="fa fa-comment" style="padding: 5px"> Contact Us</i></b-nav-item>
+          <b-nav-item to="/login"><i class="fa fa-sign-in" style="padding: 5px"> Login </i></b-nav-item>
+          <b-nav-item to="/logout"><i class="fa fa-sign-out" style="padding: 5px"> Logout </i></b-nav-item>
+          <i class="fa fa-pied-piper-alt fa-1x" style="padding: 5px; color: black;"></i>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <router-view/>
   </div>
 </template>
@@ -17,7 +36,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #3a3333;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Pets from '@/components/Pets'
+import About from '@/components/About'
+import AddPet from '@/components/AddPet'
+import Contact from '@/components/Contact'
 
 Vue.use(Router)
 
@@ -8,8 +12,28 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/pets',
+      name: 'Pets',
+      component: Pets
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    },
+    {
+      path: '/add',
+      name: 'Add Pet',
+      component: AddPet
     }
   ]
 })
